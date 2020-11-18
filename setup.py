4504da4ee1c6b793f07471cb421cbc675bdecbaf
Stijn <debackere@strw.leidenuniv.nl>
2020-11-18 15:50:22 +0100

    Ensure that we download correct version of george.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("readme.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tremulator", # Replace with your own username
-    version="0.0.4",
+    name="tremulator",
+    version="0.0.5",
     author="Stijn Debackere",
     author_email="debackere@strw.leidenuniv.nl",
     description="A package to emulate expensive functions using Gaussian processes.",
@@ -13,16 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/StijnDebackere/tremulator",
     packages=setuptools.find_packages(),
-    install_requires=["numpy",
-                      "scipy",
-                      "asdf",
-                      "emcee",
-                      "george",
-                      "pyDOE",
-                      "tqdm"],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "asdf",
+        "emcee",
+        "george @ git+https://github.com/StijnDebackere/george",
+        "pyDOE",
+        "tqdm"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ]
